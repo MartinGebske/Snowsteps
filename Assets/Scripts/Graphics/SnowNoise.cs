16f326878194f0c8783ce1e4ec051ca0ce59d2e9
Martin <martin.gebske@hotmail.de>
2018-08-22 08:07:16 +0200

    Test

---
+++ Assets/Scripts/Graphics/SnowNoise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowNoise : MonoBehaviour {
+
+    public Shader snowFallShader;
+    [Range(0.001f, 0.1f)]public float flakeAmount;
+    [Range(0f,1f)]public float flakeOpacity;
+
+
+    Material snowFallMaterial;
+    MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+        snowFallMaterial = new Material(snowFallShader);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        snowFallMaterial.SetFloat("_FlakeAmount", flakeAmount);
+        snowFallMaterial.SetFloat("_FlakeOpacity", flakeOpacity);
+        RenderTexture snow = (RenderTexture)meshRenderer.material.GetTexture("_Splat");
+        RenderTexture temp = RenderTexture.GetTemporary(snow.width, snow.height, 0, RenderTextureFormat.ARGBFloat);
+        // Creates a drawcall ie. for Bloom or Depth 
+        Graphics.Blit(snow, temp, snowFallMaterial);
+        Graphics.Blit(temp, snow);
+        meshRenderer.material.SetTexture("_Splat", snow);
+        RenderTexture.ReleaseTemporary(temp);
+	}
+}
---
+++ Assets/Scripts/Graphics/Tracks.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracks : MonoBehaviour {
+
+    public Shader drawShader;
+
+    public GameObject terrain;
+
+    public Transform[] foot;
+
+    [Range(0, 2)] public float brushSize;
+    [Range(0, 1)] public float brushStrength;
+
+    int layerMask;
+
+    RaycastHit groundHit;
+
+    Material snowMaterial, drawMaterial;
+
+    RenderTexture splatMap;
+
+
+	
+	void Start () {
+
+        layerMask = LayerMask.GetMask("Ground");
+
+        drawMaterial = new Material(drawShader);
+        drawMaterial.SetVector("_Color", Color.red);
+
+        snowMaterial = terrain.GetComponent<MeshRenderer>().material;
+
+        splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        snowMaterial.SetTexture("_Splat", splatMap);
+	}
+
+	void Update () {
+        for (int i = 0; i < foot.Length; i++)
+        {
+            if (Physics.Raycast(foot[i].position, -Vector3.up, out groundHit, 1f, layerMask))
+                {
+                drawMaterial.SetVector("_Coordinate", new Vector4(groundHit.textureCoord.x, groundHit.textureCoord.y, 0, 0));
+                    drawMaterial.SetFloat("_Strength", brushStrength);
+                    drawMaterial.SetFloat("_Size", brushSize);
+
+
+                    RenderTexture temp = RenderTexture.GetTemporary(splatMap.width, splatMap.height,
+                         0, RenderTextureFormat.ARGBFloat);
+
+                    Graphics.Blit(splatMap, temp);
+                    Graphics.Blit(temp, splatMap, drawMaterial);
+                    RenderTexture.ReleaseTemporary(temp);
+              }
+        }
+	}
+}
