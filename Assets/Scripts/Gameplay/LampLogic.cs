1a963b68ecdde74b8b2b7a2a25ca4b7744bad33b
Martin <martin.gebske@hotmail.de>
2018-08-30 07:18:54 +0200

    Lamp to parent

---
+++ Assets/Scripts/Gameplay/LampLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampLogic : MonoBehaviour {
+
+    public Transform handPosition;
+    public float offset;
+
+	void Start () {
+        gameObject.transform.SetParent(handPosition);
+        gameObject.transform.localPosition = new Vector3(0,offset,0);
+
+	}
+}
